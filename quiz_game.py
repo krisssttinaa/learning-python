0d035302824e6d554364b9f46d4759d1ad083ce1
Kristina Piiarska <117029938+krisssttinaa@users.noreply.github.com>
2023-09-25 23:32:40 +0200

    Add files via upload

---
+++ quiz_game.py
@@ -0,0 +1,21 @@
+def new_game():
+    pass
+def check_answer():
+    pass
+def display_score():
+    pass
+def play_again():
+    pass
+
+questions = {
+    "Who created Python?: ": "A",
+    "What year was Python created?: ": "B",
+    "Python is tributed to which comedy group?: ":"C",
+    "Is the Earth round?: ":"A"
+}
+
+options = [ 
+    ["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zucherburg"],
+    ["A. 1989", "B. 1991", "C. 2000", "D. 2016"]
+    ["A. Lonely Island", "B. Smoth"]
+]
