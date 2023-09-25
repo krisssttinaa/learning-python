9296ac678eefba18062229acb7334fe85b6b8302
Kristina Piiarska <117029938+krisssttinaa@users.noreply.github.com>
2023-09-25 23:47:41 +0200

    Update quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -1,9 +1,47 @@
 def new_game():
-    pass
-def check_answer():
-    pass
-def display_score():
-    pass
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
+
+    for key in questions:
+        print("------------------")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        quess = input("Enter A, B, C, D: ")
+        quess= quess.upper()
+        guesses.append(quess)
+
+        correct_guesses += check_answer(questions.get(key),guess)
+        question_num +=1
+
+    display_score(correct_guesses, guesses)
+
+def check_answer(answer, guess):
+    if answer == guess:
+        print("CORRECT!")
+        return 1
+    else:
+        print("WRONG!")
+        return 0
+    
+def display_score(correct_guesses, guesses):
+    print("--------------")
+    print("RESULTS")
+    print("--------------")
+    print("Answers: ", end="")
+    for i in questions:
+        print(questions.get(i), end="")
+    print()
+
+    print("Guesses: ", end="")
+    for i in guesses:
+        print(guesses.get(i), end=" ")
+    print()
+
+    score = int((correct_guesses/len(questions))*100)
+    print("Your score is: ")
+
 def play_again():
     pass
 
@@ -17,5 +55,8 @@ questions = {
 options = [ 
     ["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zucherburg"],
     ["A. 1989", "B. 1991", "C. 2000", "D. 2016"]
-    ["A. Lonely Island", "B. Smoth"]
+    ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+    ["A. True", "B. False", "C. Sometimes", "D. What's Earth?"]
 ]
+
+new_game()
