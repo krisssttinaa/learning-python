1dfa3b8517c0bf8dae3d8736e1e6428a1eb636d4
Kristina Piiarska <117029938+krisssttinaa@users.noreply.github.com>
2023-09-25 23:54:16 +0200

    Update quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -31,16 +31,16 @@ def display_score(correct_guesses, guesses):
     print("--------------")
     print("Answers: ", end="")
     for i in questions:
-        print(questions.get(i), end="")
+        print(questions.get(i), end=" ")
     print()
 
     print("Guesses: ", end="")
     for i in guesses:
-        print(i, end="")
+        print(i, end=" ")
     print()
 
     score = int((correct_guesses/len(questions))*100)
-    print("Your score is: ")
+    print("Your score is: "+str(score)+"%")
 
 def play_again():
     pass
