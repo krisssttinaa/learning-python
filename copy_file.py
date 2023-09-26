95245e7af28a4beb41d63e3bf98b4e17454fb734
Kristina Piiarska <117029938+krisssttinaa@users.noreply.github.com>
2023-09-27 00:24:55 +0200

    Delete copy_file.py

--- copy_file.py
+++
@@ -1,9 +0,0 @@
-# copyfile() = copies contents of a file
-# copy() =     copyfile() +permission mode +destination can be a directory
-# copy2()=     copy() +copies metadata (file's creation and modification times)
-
-import shutil
-
-shutil.copyfile("/Users/krisss/Desktop/temp.txt", "/Users/krisss/Desktop/test.txt")
-#shutil.copy("/Users/krisss/Desktop/temp.txt", "/Users/krisss/Desktop/test.txt")
-#shutil.copy2("/Users/krisss/Desktop/temp.txt", "/Users/krisss/Desktop/test.txt")
